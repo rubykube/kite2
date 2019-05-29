a69b955058e8bcf226cfb152e95a7d07ed039d1e
MoD <mod@websys.io>
2019-05-29 10:01:41 +0300

    minor changes

--- lib/kite.rb
+++ lib/kite.rb
@@ -1,4 +1,7 @@
+require 'gli'
+
 require 'kite/version'
+require 'kite/app'
 
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
